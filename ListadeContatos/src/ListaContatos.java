738b639916e969a61425f9ce1596c5a50af0e2e2
Bruno Venzo Debacco <136653751+RezidenteGamer@users.noreply.github.com>
2024-11-18 02:28:21 -0300

    Atualização de legibilidade

--- ListadeContatos/src/ListaContatos.java
+++ ListadeContatos/src/ListaContatos.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.awt.event.*;
 import java.util.ArrayList;
 
-public class ListaContatos {
+public class ListaContatos { //inicializa os elementos
     private JPanel tela;
     private JTextField campoNome;
     private JTextField campoTelefone;
@@ -13,17 +13,16 @@ public class ListaContatos {
     private DefaultListModel<String> listModel;
 
     public ListaContatos() {
-        listModel = new DefaultListModel<>();
+        listModel = new DefaultListModel<>(); //Define como sera a lista/modelo de lista
         listaDeContatos.setModel(listModel);
 
-        adiconarContatoBut.addActionListener(new ActionListener() {
-            @Override
+        adiconarContatoBut.addActionListener(new ActionListener() { //detecta o clique e tenta pegar as informações passadas pelo usuario
             public void actionPerformed(ActionEvent e) {
                 String nome = campoNome.getText();
                 String telefone = campoTelefone.getText();
                 String email = campoEmail.getText();
 
-                if(nome.isEmpty() || telefone.isEmpty() || email.isEmpty()) {
+                if(nome.isEmpty() || telefone.isEmpty() || email.isEmpty()) { //se algum estiver vazio exibe mensagem de erro!
                     JOptionPane.showMessageDialog(tela, "Preencha todos os campos!", "Erro", JOptionPane.ERROR_MESSAGE);
                     return;
                 }
@@ -37,13 +36,14 @@ public class ListaContatos {
         });
 
         removerContatoBut.addActionListener(new ActionListener() {
-            @Override
             public void actionPerformed(ActionEvent e) {
-                int selectedIndex = listaDeContatos.getSelectedIndex();
-                if(selectedIndex != -1) {
-                    listModel.remove(selectedIndex);
+                int contatoSelecionado = listaDeContatos.getSelectedIndex();
+                if(contatoSelecionado != -1) {
+                    listModel.remove(contatoSelecionado);
                 } else {
                     JOptionPane.showMessageDialog(tela, "Selecione um contato para remover.", "Erro", JOptionPane.ERROR_MESSAGE);
+                    //Aciona uma caixa de texto que mostra ao usuario que ele nao selecionou nada para remover!!
+
                 }
             }
         });
@@ -53,7 +53,7 @@ public class ListaContatos {
         JFrame frame = new JFrame("Cadastro de Contatos");
         ListaContatos listaContatos = new ListaContatos();
         frame.setContentPane(listaContatos.tela);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Encerra o codigo assim que ajanela é fechada
         frame.pack();
         frame.setVisible(true);
     }
